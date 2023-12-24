e3859a800c7cd4dc3462e95da69f045d5a4a4911
dhilip <dhiliptry7@gmail.com>
2023-12-24 14:48:27 +0000

    solution

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+create table if not exists category (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255),
+    description varchar(255)
+);
+
+create table if not exists product(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255),
+    description varchar(255),
+    price Double
+);
